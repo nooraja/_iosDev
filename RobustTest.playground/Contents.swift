737ed7ab692f0c668796ff9aa3c8949132165f61
noor <muhammadnoor007@gmail.com>
2016-12-23 23:05:04 +0700

    reverse word project

---
+++ RobustTest.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let str = "Hello"
+var n = str.characters.count - 1;
+var m = 0;
+
+// Alhamdulilah , i got it now,
+// Finally i understand
+while m <= n {
+    
+    //var six = str.index(str.startIndex, offsetBy: n)
+    var lastChar: Character = str[str.index(str.startIndex, offsetBy: n)]
+    print(lastChar)
+    n = n - 1;
+}
+
+
+//###//
+var mealCost:Double = 3.5
+var tip:Int = 20;
+
+var tipCost:Double = ( mealCost * Double(tip) ) / 100.0
+var total: Double = mealCost + tipCost
+
+print(total)
+
